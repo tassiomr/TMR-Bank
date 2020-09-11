0749ce3f544e79643b31f3ace4b5d1b8f3db7f2b
Tassio Rocha <tassiorocha@frwk.com.br>
2020-09-11 09:20:59 -0300

    Initial Commit

---
+++ TMR Bank/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TMR Bank
+//
+//  Created by Tássio Rocha on 11/09/20.
+//  Copyright © 2020 Tássio Rocha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TMR BankTests/TMR_BankTests.swift
@@ -0,0 +1,34 @@
+//
+//  TMR_BankTests.swift
+//  TMR BankTests
+//
+//  Created by Tássio Rocha on 11/09/20.
+//  Copyright © 2020 Tássio Rocha. All rights reserved.
+//
+
+import XCTest
+@testable import TMR_Bank
+
+class TMR_BankTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
