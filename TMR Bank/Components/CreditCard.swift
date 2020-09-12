fc5d29c567474f1c45f39c4c2520b434e3ef5648
Tassio Rocha <tassiomarcosr@outlook.com>
2020-09-12 11:38:31 -0300

    feat: create component to show credit card info

---
+++ TMR Bank/Components/CreditCard.swift
@@ -0,0 +1,64 @@
+//
+//  CreditCard.swift
+//  TMR Bank
+//
+//  Created by Tássio Rocha on 11/09/20.
+//  Copyright © 2020 Tássio Rocha. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreditCard: View {
+    var cardNumber: String
+    var balance: String
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 35) {
+            HStack(alignment: .center, spacing: 55){
+                Text(cardNumber)
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                                    .offset(x: -48)
+                HStack {
+                Circle()
+                    .fill(Color.yellow)
+                    .frame(width: 25, height: 25)
+                    .shadow(radius: 15)
+                                    .offset(x: -64)
+                
+                Circle()
+                                        .offset(x: -84)
+                    .fill(Color.red)
+                    .frame(width: 25, height: 25)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 1).shadow(radius: 1).offset(x: -84))
+                }
+            }.frame(width: UIScreen.main.bounds.width * 0.90)
+            
+            VStack {
+            Text("Balance")
+                .font(.caption)
+                .fontWeight(.bold)
+                .foregroundColor(.gray)
+                .offset(x: 15, y: 10)
+            
+            
+            Text("$" + balance)
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
+                .offset(x: 32, y: 12)
+            }
+            
+        }.frame(width: UIScreen.main.bounds.width * 0.90, height: 180)
+            .background(Color.white)
+            .cornerRadius(30)
+            .shadow(color: .gray, radius: 5, x: 0, y: 0)
+    }
+}
+
+struct CreditCard_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditCard(cardNumber: "111133344", balance: "4000,00")
+    }
+}
